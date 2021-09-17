7a4cdc8232b64c14ccc1b46eb7a022186c1a29c3
Adebayo Toyosi <adebayo.toyosi@deimos.co.za>
2021-09-17 20:52:44 +0100

    adds more changes to file

--- src/components/football-data/index.js
+++ src/components/football-data/index.js
@@ -19,12 +19,23 @@ export default class FootballMatchesData extends Component {
       selectedYear: year
     })
     fetch(`https://jsonmock.hackerrank.com/api/football_competitions?year=${year}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
+      .then(response =>
+        response.json())
+      .then(data => {
+        console.log(data)
+        this.renderMatches(data.data[0])
+      });
+  }
+
+  renderMatches = (matchInfo) => {
+    const matchMarkup = document.querySelector('#matches');
+    let element = document.createElement('li');
+    element.textContent = `Match ${matchInfo.name} won by  ${matchInfo.winner}`;
+    matchMarkup.appendChild(element);
   }
 
   render() {
-    var years= [2011, 2012, 2013, 2014, 2015, 2016, 2017];
+    var years = [2011, 2012, 2013, 2014, 2015, 2016, 2017];
     return (
       <div className="layout-row">
         <div className="section-title">Select Year</div>
@@ -37,8 +48,8 @@ export default class FootballMatchesData extends Component {
                   'active': this.state.selectedYear === year
                 })
               }
-              onClick={this.onClick(year)}
-              key={year}>
+                onClick={this.onClick(year)}
+                key={year}>
                 <a>{year}</a>
               </li>
             )
@@ -48,8 +59,8 @@ export default class FootballMatchesData extends Component {
         <section className="content">
           <section>
             <div className="total-matches" data-testid="total-matches"></div>
-            
-            <ul className="mr-20 matches styled" data-testid="match-list">
+
+            <ul className="mr-20 matches styled" id="matches" data-testid="match-list">
               <li className="slide-up-fade-in"> </li>
             </ul>
           </section>
